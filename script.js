b311c1d9a71d5bc5cac3f15ccc3e8c05485d262e
Haelina <tarany@hotmail.fr>
2023-11-09 16:17:05 -0600

    - Corrected id for confirm password - User input is collected in userInfo object - Input field reset to empty after submission - Prevent reloading of page when submitting - Form submitted only if both password fields match

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-const firstName = document.getElementById('first-name');
-const lastName = document.getElementById('last-name');
-const email = document.getElementById('email');
-const phone = document.getElementById('phone');
-const password = document.getElementById('password');
-const confirmedPassword = getElementById('confirmed-password');
+const firstNameInput = document.getElementById('first-name');
+const lastNameInput = document.getElementById('last-name');
+const emailInput = document.getElementById('email');
+const phoneInput = document.getElementById('phone');
+const passwordInput = document.getElementById('password');
+const confirmedPasswordInput = document.getElementById('confirmed-password');
 const button = document.getElementById('submit-button');
 
 function submitHandler() {
-    console.log(firstName.value);
+    event.preventDefault();
+    if (passwordInput.value == confirmedPasswordInput.value) {
+        const formInputs = {
+            firstName: firstNameInput.value,
+            lastName: lastNameInput.value,
+            email: emailInput.value,
+            phone: phoneInput.value,
+            password: passwordInput.value
+        }
+        const userInfo = document.querySelectorAll("input");
+        userInfo.forEach(info => info.value="");
+        console.log(formInputs);
+    }
 }
 
-button.addEventListener('click', submitHandler);
+    button.addEventListener('click', submitHandler);
